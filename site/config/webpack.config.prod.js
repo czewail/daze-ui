a1b7178b1b398857d6dd607a8ce78f9d1260a10c
陈泽韦 <chenzewei@zhujia360.com>
2018-03-27 16:36:50 +0800

    gh-pages

--- site/config/webpack.config.prod.js
+++ site/config/webpack.config.prod.js
@@ -1,11 +1,31 @@
-const path = require('path');
-const webpack = require('webpack');
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
 
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, '../src'); // __dirname 中的src目录，以此类推
-var COMPONENT_PATH = path.resolve(ROOT_PATH, '../../src'); // __dirname 中的src目录，以此类推
-var APP_FILE = path.resolve(APP_PATH, 'index.js'); // 根目录文件app.jsx地址
-var BUILD_PATH = path.resolve(ROOT_PATH, '../dist/assets/'); // 发布文件所存放的目录
+const pkgPath = path.join(path.resolve(__dirname, '../../'), 'package.json')
+
+const pkg = fs.existsSync(pkgPath) ? require(pkgPath) : {}
+
+const ROOT_PATH = path.resolve(__dirname)
+const APP_PATH = path.resolve(ROOT_PATH, '../src') // __dirname 中的src目录，以此类推
+const COMPONENT_PATH = path.resolve(ROOT_PATH, '../../src') // __dirname 中的src目录，以此类推
+const APP_FILE = path.resolve(APP_PATH, 'index.js') // 根目录文件app.jsx地址
+const BUILD_PATH = path.resolve(ROOT_PATH, '../dist/assets/') // 发布文件所存放的目录
+
+let theme = {}
+if (pkg.theme && typeof (pkg.theme) === 'string') {
+  let cfgPath = pkg.theme
+  // relative path
+  if (cfgPath.charAt(0) === '.') {
+    cfgPath = path.resolve('../../', cfgPath)
+  }
+  const getThemeConfig = require(cfgPath)
+  theme = getThemeConfig()
+} else if (pkg.theme && typeof (pkg.theme) === 'object') {
+  /* eslint prefer-destructuring: 0 */
+  theme = pkg.theme
+}
 
 module.exports = {
   entry: APP_FILE,
@@ -14,8 +34,8 @@ module.exports = {
     // 输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
     // “path”仅仅告诉Webpack结果存储在哪里
     filename: 'bundle.js',
-    publicPath: "assets/",
-    //模板、样式、脚本、图片等资源对应的server上的路径
+    publicPath: 'assets/',
+    // 模板、样式、脚本、图片等资源对应的server上的路径
     // “publicPath”项则被许多Webpack的插件用于在生产模式下更新内嵌到css、html文件里的url值。
   },
   module: {
@@ -23,67 +43,73 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader',
+          'css-loader?sourceMap',
           {
             loader: 'postcss-loader',
             options: {
-            plugins: (loader) => [
-              require('autoprefixer')(),
-              // require('cssnano')()
-              ]
-            }
-          }
-        ]
+              sourceMap: 'inline',
+              plugins: () => [
+                autoprefixer({
+                  browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
+                }),
+                // require('cssnano')()
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.less$/,
         use: [
           'style-loader',
-          'css-loader',
+          'css-loader?sourceMap',
           {
             loader: 'postcss-loader',
             options: {
-              plugins: (loader) => [
-                require('autoprefixer')(),
+              sourceMap: 'inline',
+              plugins: () => [
+                autoprefixer({
+                  browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
+                }),
                 // require('cssnano')()
-              ]
-            }
+              ],
+            },
           },
-          'less-loader'
-        ]
+          `less-loader?{"sourceMap":true,"modifyVars":${JSON.stringify(theme)}}`,
+        ],
       },
       {
         test: /\.(eot|woff|svg|ttf|woff2|gif)(\?|$)/,
-        loader: 'file-loader?name=[hash].[ext]'
+        loader: 'file-loader?name=[hash].[ext]',
       },
       {
         test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=1200&name=[hash].[ext]'
+        loader: 'url-loader?limit=1200&name=[hash].[ext]',
       },
       {
         test: /\.md$/,
-        loader : 'raw-loader'
-      }
-    ]
+        loader: 'raw-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production') //定义编译环境
-      }
+        NODE_ENV: JSON.stringify('production'), // 定义编译环境
+      },
     }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.less', '.scss', '.css'], //后缀名自动补全
+    extensions: ['.js', '.jsx', '.less', '.scss', '.css'], // 后缀名自动补全
     alias: {
-      "@": APP_PATH,
-      "component_src": COMPONENT_PATH
-    }
-  }
+      '@': APP_PATH,
+      '~components': COMPONENT_PATH,
+    },
+  },
 }
